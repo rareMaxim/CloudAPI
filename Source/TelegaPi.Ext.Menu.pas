b625e51968ebf5ead7d42a418e70da5308ff5a28
Sysoev Maxim <maks4a@gmail.com>
2018-02-19 16:50:22 +0200

    Черновичек

---
+++ Source/TelegaPi.Ext.Menu.pas
@@ -0,0 +1,54 @@
+﻿unit TelegaPi.Ext.Menu;
+////////////////////////////////////////////////////////////////////////////////
+///
+///  ЧЕРНОВИК! Класс для создания навигации в боте
+///
+///  ///////////////////////////////////////////////////////////////////////////
+
+interface
+
+uses
+  System.Generics.Collections,
+  System.SysUtils;
+
+type
+  TTgMenu = class
+  private
+    FSubMenu: TDictionary<string, TTgMenu>;
+    FOnOpen: TProc;
+    FName: string;
+  protected
+    procedure DoOpen;
+  public
+    procedure NewSubMenu(AMenu: TTgMenu);
+    function Navigate(const AName: string): Boolean;
+    property Name: string read FName write FName;
+    property OnOpen: TProc read FOnOpen write FOnOpen;
+  end;
+
+implementation
+
+{ TTgMenu }
+
+procedure TTgMenu.DoOpen;
+begin
+  if Assigned(OnOpen) then
+    OnOpen(); // Запускаем связаное событие
+end;
+
+function TTgMenu.Navigate(const AName: string): Boolean;
+begin
+  Result := FSubMenu.ContainsKey(AName);
+  if Result then   // Если зарегистрировано вложеное меню
+    FSubMenu[AName].DoOpen; // Запускаем обработчик
+end;
+
+procedure TTgMenu.NewSubMenu(AMenu: TTgMenu);
+begin
+  if FSubMenu.ContainsKey(AMenu.Name) then //если есть уже есть такое имя
+    FSubMenu.Remove(AMenu.Name); //убираем раннее созданый элемент
+  FSubMenu.Add(AMenu.Name, AMenu);
+end;
+
+end.
+
