ada9130eb2b4e2dd771310d7dfad4e3dad814659
MAXIM-SYSOEV\WorkPC <work@rare.max>
2020-07-17 15:28:01 +0300

    Немного улучшил класс ECloudApiException

--- CloudAPI.Exceptions.pas
+++ CloudAPI.Exceptions.pas
@@ -7,11 +7,23 @@ uses
   System.SysUtils;
 
 type
-  ECloudApiException = class(Exception);
+  ECloudApiException = class(Exception)
+  private
+    FCodeInt: Integer;
+    FCodeStr: string;
+    FMessage: string;
+  public
+    constructor Create(const ACode, AMessage: string); overload;
+    constructor Create(const ACode: Integer; const AMessage: string); overload;
+  published
+    property CodeInt: Integer read FCodeInt write FCodeInt;
+    property CodeStr: string read FCodeStr write FCodeStr;
+    property Message: string read FMessage write FMessage;
+  end;
 
   ECloudApiRequairedParameterException = class(ECloudApiException)
   private const
-    C_MESSAGE = 'Parameter %s is requaired!';
+    C_MESSAGE = 'Parameter "%s" is requaired!';
   public
     constructor Create(AParameter: TcaParameter);
   end;
@@ -22,7 +34,22 @@ implementation
 
 constructor ECloudApiRequairedParameterException.Create(AParameter: TcaParameter);
 begin
-  inherited CreateFmt(C_MESSAGE, [AParameter.Name]);
+  inherited Create('CloudAPI', Format(C_MESSAGE, [AParameter.Name]));
+end;
+
+{ ECloudApiException }
+
+constructor ECloudApiException.Create(const ACode, AMessage: string);
+begin
+  FCodeStr := ACode;
+  TryStrToInt(ACode, FCodeInt);
+  FMessage := AMessage;
+  inherited CreateFmt('[%s] %s', [FCodeStr, FMessage]);
+end;
+
+constructor ECloudApiException.Create(const ACode: Integer; const AMessage: string);
+begin
+  Self.Create(ACode.ToString, AMessage);
 end;
 
 end.
