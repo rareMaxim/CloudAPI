b8460003ce08543893ac05c2f0ac0d105f8d38d6
MAXIM-SYSOEV\WorkPC <work@rare.max>
2020-08-07 09:11:42 +0300

    TcaFileToSendTag -> TcaFileToSendType

--- CloudAPI.Types.pas
+++ CloudAPI.Types.pas
@@ -7,19 +7,22 @@ uses
 
 type
 {$SCOPEDENUMS ON}
-  TcaFileToSendTag = (Error = 254, Unknown = 0, ID = 100, FromURL = 101, FromFile = 102, FromStream = 103);
   TcaParameterType = (Cookie, GetOrPost, UrlSegment, HttpHeader, RequestBody, QueryString, QueryStringWithoutEncode);
   TcaMethod = (GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH, MERGE, COPY);
 {$SCOPEDENUMS OFF}
 
   TcaFileToSend = record
+  private type
+{$SCOPEDENUMS ON}
+    TcaFileToSendType = (Error = 254, Unknown = 0, ID = 100, URL = 101, &File = 102, Stream = 103);
+{$SCOPEDENUMS OFF}
   private
     FData: string;
     FContent: TStream;
-    FTag: TcaFileToSendTag;
+    FType: TcaFileToSendType;
     FName: string;
   private
-    class function TestString(const AValue: string): TcaFileToSendTag; static;
+    class function TestString(const AValue: string): TcaFileToSendType; static;
 {$REGION 'operator overload'}
   public
     class operator Equal(a, b: TcaFileToSend): Boolean;
@@ -30,11 +33,11 @@ type
     function FileName: string;
     property Data: string read FData write FData;
     property Content: TStream read FContent write FContent;
-    property Tag: TcaFileToSendTag read FTag write FTag;
+    property &Type: TcaFileToSendType read FType write FType;
     property Name: string read FName write FName;
 
     class function Create(const AData: string; AContent: TStream;
-      const ATag: TcaFileToSendTag = TcaFileToSendTag.Unknown): TcaFileToSend; static;
+      const ATag: TcaFileToSendType = TcaFileToSendType.Unknown): TcaFileToSend; static;
     class function FromFile(const AFileName: string): TcaFileToSend; static;
     class function FromID(const AID: string): TcaFileToSend; static;
     class function FromURL(const AUrl: string): TcaFileToSend; static;
@@ -71,21 +74,21 @@ uses
 { TtgFileToSend }
 
 class function TcaFileToSend.Create(const AData: string; AContent: TStream;
-  const ATag: TcaFileToSendTag = TcaFileToSendTag.Unknown): TcaFileToSend;
+  const ATag: TcaFileToSendType = TcaFileToSendType.Unknown): TcaFileToSend;
 begin
-  Result.Tag := ATag;
+  Result.&Type := ATag;
   Result.Data := AData;
   Result.Content := AContent;
 end;
 
 class function TcaFileToSend.Empty: TcaFileToSend;
 begin
-  Result := TcaFileToSend.Create('', nil, TcaFileToSendTag.Error);
+  Result := TcaFileToSend.Create('', nil, TcaFileToSendType.Error);
 end;
 
 class operator TcaFileToSend.Equal(a, b: TcaFileToSend): Boolean;
 begin
-  Result := (a.Data = b.Data) and (a.Tag = b.Tag) and (a.Content = b.Content);
+  Result := (a.Data = b.Data) and (a.&Type = b.&Type) and (a.Content = b.Content);
 end;
 
 function TcaFileToSend.FileName: string;
@@ -100,12 +103,12 @@ class function TcaFileToSend.FromFile(const AFileName: string): TcaFileToSend;
 begin
   if not FileExists(AFileName) then
     raise EFileNotFoundException.CreateFmt('File %S not found!', [AFileName]);
-  Result := TcaFileToSend.Create(AFileName, nil, TcaFileToSendTag.FromFile);
+  Result := TcaFileToSend.Create(AFileName, nil, TcaFileToSendType.&File);
 end;
 
 class function TcaFileToSend.FromID(const AID: string): TcaFileToSend;
 begin
-  Result := TcaFileToSend.Create(AID, nil, TcaFileToSendTag.ID);
+  Result := TcaFileToSend.Create(AID, nil, TcaFileToSendType.ID);
 end;
 
 class function TcaFileToSend.FromStream(const AContent: TStream; const AFileName: string): TcaFileToSend;
@@ -117,25 +120,25 @@ begin
     raise Exception.Create('TtgFileToSend: Filename is empty!');
   if not Assigned(AContent) then
     raise EStreamError.Create('Stream not assigned!');
-  Result := TcaFileToSend.Create(AFileName, AContent, TcaFileToSendTag.FromStream);
+  Result := TcaFileToSend.Create(AFileName, AContent, TcaFileToSendType.Stream);
 end;
 
 class function TcaFileToSend.FromURL(const AUrl: string): TcaFileToSend;
 begin
-  Result := TcaFileToSend.Create(AUrl, nil, TcaFileToSendTag.FromURL);
+  Result := TcaFileToSend.Create(AUrl, nil, TcaFileToSendType.URL);
 end;
 
 class operator TcaFileToSend.Implicit(const AValue: string): TcaFileToSend;
 begin
   Result.Content := nil;
   Result.Data := AValue;
-  Result.Tag := TestString(AValue);
+  Result.&Type := TestString(AValue);
 end;
 
 class operator TcaFileToSend.Implicit(AValue: TStream): TcaFileToSend;
 begin
   Result.Content := AValue;
-  Result.Tag := TcaFileToSendTag.FromStream;
+  Result.&Type := TcaFileToSendType.Stream;
 end;
 
 function TcaFileToSend.IsEmpty: Boolean;
@@ -143,14 +146,14 @@ begin
   Result := Data.IsEmpty and not Assigned(Content);
 end;
 
-class function TcaFileToSend.TestString(const AValue: string): TcaFileToSendTag;
+class function TcaFileToSend.TestString(const AValue: string): TcaFileToSendType;
 begin
   if FileExists(AValue) then
-    Result := TcaFileToSendTag.FromFile
+    Result := TcaFileToSendType.&File
   else if AValue.contains('://') then
-    Result := TcaFileToSendTag.FromURL
+    Result := TcaFileToSendType.URL
   else
-    Result := TcaFileToSendTag.ID;
+    Result := TcaFileToSendType.ID;
 end;
 
 { TcaRequestLimit }
